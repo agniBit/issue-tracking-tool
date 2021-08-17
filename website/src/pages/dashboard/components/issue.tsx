b7c84ba9f5b7f6fb7067052526eef97c2239f7ce
Abhishek Agnihotri <agni.abhi00@gmail.com>
2021-08-17 23:10:56 +0530

    add issue section in dashboard

--- website/src/pages/dashboard/components/issue.tsx
+++ website/src/pages/dashboard/components/issue.tsx
@@ -1,14 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios, { AxiosRequestConfig } from "axios";
-
+import '../css/issue.scss';
 
 function Comments(props: { commentData: any }) {
   return (
     <div className='comment_row'>
-        {props.commentData.id}
+      <div className='user_image'>
+        <img src='/assets/user.png' alt='user' />
+      </div>
+      <div className='comment_box'>
+        <div className='comment_header'>
+          <div className='commented_by'>
+            <h5>{props.commentData.commentedBy.username}</h5>
+          </div>
+          <div className='user_role'>
+            <h5>sr. developer</h5>
+          </div>
+        </div>
+        <div className='comment_text'>
+          <p>{props.commentData.comment}</p>
+        </div>
+        <div className='comment_footer'>
+          <p>16th aug, 2021</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default function ShowIssue(props: any) {
@@ -66,24 +84,30 @@ export default function ShowIssue(props: any) {
   },[accessToken, issueId, limit, skip])
 
   return (
-    <div className='dashboard'>
-      <div className='issueId'>{issueData.issueId}</div>
-      <div className='title'>{issueData.title}</div>
-      <div className='description'>{issueData.description}</div>
-      <div className='actions'>
-        {
-          (commentData) ?
-            commentData.map((data: any, i: number) => {
-            return (<Comments commentData={data} key={i} />)
-            }) :
-            <div>loading</div>
-        }
+    <div className='issueSection'>
+      <div className='issueId'>
+        <p>{`IssueId -> ${issueData.issueId}`}</p>
+        <div className='copyClipboardlogo'>
+          <img src='/assets/copytoclipboard.png' alt='copy' />
+          <div className='info_box'>
+            <h5>Copy to clipboard</h5>
+          </div>
+        </div>
       </div>
-      bigbeurbdvc
+      <div className='title'><h3>{issueData.title}</h3></div>
+      <div className='description'><p>{issueData.decrpition}</p></div>
+      <div className='actions'>
+        <h4>Actions</h4>
+        <div className='actions_wrapper'>
+          {
+            (commentData) ?
+              commentData.map((data: any, i: number) => {
+              return (<Comments commentData={data} key={i} />)
+              }) :
+              <div>loading</div>
+          }
+        </div>
+      </div>
     </div>
   )
-}
-
-function commentData(commentData: any): string {
-  throw new Error("Function not implemented.");
 }
